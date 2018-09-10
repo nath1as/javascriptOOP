a25d57a08fc827d5f857946029558d5c4afa4bb1
nathias <n@th1.as>
2018-09-10 12:50:37 +0200

    invoice object completed

--- invoices.js
+++ invoices.js
@@ -15,11 +15,38 @@ var invoices = {
     }
     return sum;
   },
+  paid: [],
+  payInvoice: function(name) {
+    var unpaid = [];
+    var i;
+
+    for (i = 0; i < this.unpaid.length; i++) {
+      if (name === this.unpaid[i].name) {
+        this.paid.push(this.unpaid[i]);
+      } else {
+        unpaid.push(this.unpaid[i]);
+      }
+    }
+
+    this.unpaid = unpaid;
+  },
+  totalPaid: function () {
+    var sum = 0;
+    var i;
+
+    for (i = 0; i < this.paid.length; i++) {
+      sum += this.paid[i].amount;
+    }
+    return sum;
+  },
 };
 
 invoices.add("Due North Development", 250);
 invoices.add("Moonbeam Interactive", 187.50);
 invoices.add("Slough Digital", 300);
-console.log(invoices.totalDue());
+invoices.payInvoice("Due North Development");
+invoices.payInvoice("Slough Digital");
 
+console.log(invoices.totalDue());
+console.log(invoices.totalPaid());
 console.log(invoices);
