56635a0d821bb9cd4c3f27005e348e4e35cd0d53
nathias <n@th1.as>
2018-09-14 10:32:34 +0200

    factory implemented

---
+++ countryObjectFactory.js
@@ -0,0 +1,19 @@
+function makeCountry(country, continent) {
+  return {
+    name: country,
+    continent: continent,
+    getDescription: function() {
+      return this.name + ' is located in ' + this.continent + '.';
+    }
+  };
+}
+
+var chile = makeCountry('The Republic of Chile', 'South America');
+var canada = makeCountry('Canada', 'North America');
+var southAfrica = makeCountry('The Republic of South Africa', 'Africa');
+
+console.log(
+  chile.getDescription(),       // "The Republic of Chile is located in South America."
+  canada.getDescription(),      // "Canada is located in North America."
+  southAfrica.getDescription(), // "The Republic of South Africa is located in Africa."
+);
